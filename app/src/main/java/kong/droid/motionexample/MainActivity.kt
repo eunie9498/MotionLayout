975d9384bd0eaf993b3b9ceb5cf6897101a4c9c0
eunie9498 <bvegemilb@naver.com>
2022-06-01 22:21:18 +0900

    Appbar custom. collapsingToolbar

--- app/src/main/java/kong/droid/motionexample/MainActivity.kt
+++ app/src/main/java/kong/droid/motionexample/MainActivity.kt
@@ -1,14 +1,22 @@
 package kong.droid.motionexample
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import kong.droid.motionexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
     //애니메이션 동작 여부 체크
     var alreadyAnimation = false
@@ -17,22 +25,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        makeStatusTrans()
+        initActionBar()
+        initInset()
         initView()
+    }
+
+    fun initInset()= with(binding) {
+        ViewCompat.setOnApplyWindowInsetsListener(coordinator){ v:View, insets: WindowInsetsCompat ->
+            val params = v.layoutParams as ViewGroup.MarginLayoutParams
+            params.bottomMargin = insets.systemWindowInsetBottom
+            toolbarContainer.layoutParams = (toolbarContainer.layoutParams as ViewGroup.MarginLayoutParams).apply {
+                setMargins(0,insets.systemWindowInsetTop,0,0)
+            }
+            collapsiongTool.layoutParams= (collapsiongTool.layoutParams as ViewGroup.MarginLayoutParams).apply {
+                setMargins(0,0,0,0)
+            }
+            insets.consumeSystemWindowInsets()
+        }
+    }
+
+    fun initActionBar() = with(binding){
+        toolbar.navigationIcon = null
+        toolbar.setContentInsetsAbsolute(0,0)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.let {
+            it.setHomeButtonEnabled(false)
+            it.setDisplayHomeAsUpEnabled(false)
+            it.setDisplayShowHomeEnabled(false)
+        }
     }
 
     private fun initView() = with(binding) {
 
 
         scrollView.viewTreeObserver.addOnScrollChangedListener {
-            if(scrollView.scrollY > 150f.dpToPx()) {
-                if(alreadyAnimation.not()) {
+            if (scrollView.scrollY > 150f.dpToPx()) {
+                if (alreadyAnimation.not()) {
                     motionLayout.transitionToEnd()
                     wholeMotion.transitionToEnd()
                 }
-            }
-            else {
-                if(alreadyAnimation.not()) {
+            } else {
+                if (alreadyAnimation.not()) {
                     motionLayout.transitionToStart()
                     wholeMotion.transitionToStart()
                 }
@@ -57,7 +91,8 @@ class MainActivity : AppCompatActivity() {
                 startId: Int,
                 endId: Int,
                 progress: Float
-            )  = Unit
+            ) = Unit
+
             override fun onTransitionTrigger(
                 motionLayout: MotionLayout?,
                 triggerId: Int,
@@ -67,7 +102,21 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    fun Float.dpToPx() : Float {
-        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, this@MainActivity.resources.displayMetrics)
+    fun Float.dpToPx(): Float {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            this,
+            this@MainActivity.resources.displayMetrics
+        )
+    }
+
+    fun Activity.makeStatusTrans() {
+        window.apply {
+            decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            statusBarColor = Color.TRANSPARENT
+        }
     }
 }
